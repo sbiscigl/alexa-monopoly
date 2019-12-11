dfbb91b70dcb83dd9794563ee72eaa02f2f8d811
O'laughlin <doolaugh@f0189814e7e8.ant.amazon.com>
2019-12-11 13:32:27 -0500

    added logic to charge player if they land on an owned property

--- src/main/java/com/amazon/sbidoo/game/MonopolyGameHandler.java
+++ src/main/java/com/amazon/sbidoo/game/MonopolyGameHandler.java
@@ -14,6 +14,7 @@ import com.amazon.sbidoo.game.status.SpaceInfo;
 import com.google.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.Map;
 
 import java.util.Optional;
 import java.util.Set;
@@ -46,11 +47,103 @@ public class MonopolyGameHandler implements GameHandler {
         Player playerOnTurn = getPlayerOnTurn(userId);
         if(playerOnTurn != null) {
             playerOnTurn.updatePositionFromStart(dieOne, dieTwo);
-            //check if someone owns the spot you just landed on
+            chargePlayerIfSpaceIsOwned(userId, playerOnTurn);
         }
         else {
             System.out.println("There are no active players in the game...");
         }
+    }
+
+
+    private void chargePlayerIfSpaceIsOwned(String userId, Player playerOnTurn) {
+        final GameStatus gameStatusForUserId = this.gameStatusDao.getGameStatusForUserId(userId);
+        Property property = gameStatusForUserId.getProperty();
+        Board board = getGameBoard(userId);
+        int propertyIndex = playerOnTurn.getPositionFromStart();
+        Space space = board.getSpaceMap().get(propertyIndex);
+
+        Space.SpaceType spaceType = board.getSpaceMap().get(propertyIndex).getSpaceType();
+        Space.SpaceCategory spaceCategory = board.getSpaceMap().get(propertyIndex).getSpaceCategory();
+        String spaceName = board.getSpaceMap().get(propertyIndex).getSpaceName();
+
+        //This is the key to the propertyMap
+        SpaceInfo spaceInfo  = new SpaceInfo(spaceType, spaceCategory, spaceName);
+        Map<SpaceInfo, OwnerInfo> propertyMap = property.getPropertyMap();
+
+        if(propertyMap.containsKey(spaceInfo)){
+            OwnerInfo propertyOwnerInfo = property.getPropertyMap().get(spaceInfo);
+            Player.PieceType ownerPieceType = propertyOwnerInfo.getOwner();
+            int spacePrice = space.getPrice();
+            int houses = propertyOwnerInfo.getHouses();
+            int hotels = propertyOwnerInfo.getHotels();
+
+            //Alexa should say this
+            System.out.println("This property is owned by " + ownerPieceType + ".");
+            int chargePrice = calculateChargePrice(spacePrice, houses, hotels, propertyMap, spaceType, spaceCategory, ownerPieceType);
+            int playerMoney = playerOnTurn.getMoney();
+            if(playerMoney >= chargePrice) {
+                //Alexa should say this
+                System.out.println("Charging you " + chargePrice + " dollars.");
+                playerMoney -= chargePrice;
+                playerOnTurn.updateMoney(playerMoney);
+                //Alexa should say this
+                System.out.println("Your new balance is " + playerMoney + " dollars.");
+                return;
+            }
+            else {
+                //Alexa should say this
+                System.out.println("You don't have enough money to pay me, I win!");
+                return;
+            }
+
+        }
+    }
+
+    private int calculateChargePrice(int spacePrice,
+                                     int houses,
+                                     int hotels,
+                                     Map<SpaceInfo, OwnerInfo> propertyMap,
+                                     Space.SpaceType spaceType,
+                                     Space.SpaceCategory spaceCategory,
+                                     Player.PieceType ownerPieceType) {
+        //Check if owner has color set
+        boolean hasColorSet = checkForColorSet(propertyMap, spaceType, spaceCategory, ownerPieceType);
+
+        int chargePrice;
+        if(!hasColorSet) {
+            chargePrice = (int) Math.floor(spacePrice * 0.07);
+            return chargePrice;
+        }
+        else {
+            //This assumes no houses or hotels have been built
+            //Add logic later for if house and hotels have been built
+            chargePrice = (int) Math.floor(spacePrice * 0.15);
+            return chargePrice;
+        }
+
+    }
+
+    private boolean checkForColorSet(Map<SpaceInfo, OwnerInfo> propertyMap, Space.SpaceType spaceType, Space.SpaceCategory spaceCategory, Player.PieceType ownerPieceType) {
+        int counter = 0;
+        boolean hasColorSet = false;
+        for(Map.Entry<SpaceInfo, OwnerInfo> entry : propertyMap.entrySet()){
+            if(entry.getKey().getSpaceCategory() == spaceCategory
+                    && entry.getKey().getSpaceType() == spaceType
+                    && entry.getValue().getOwner() == ownerPieceType) {
+                counter++;
+            }
+        }
+
+        //For most properties, three spaces constitutes a color set
+        if(counter == 3) {
+            hasColorSet = true;
+        }
+
+        //For the brown and blue properties, two spaces constitutes a color set
+        if(counter == 2 && (spaceCategory == Space.SpaceCategory.Brown || spaceCategory == Space.SpaceCategory.Blue)) {
+            hasColorSet = true;
+        }
+        return hasColorSet;
     }
 
     private void buyProperty(String userId) {
