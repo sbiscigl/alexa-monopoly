87003d8fc421bd498bb0156a49899358c387e92d
sbiscigl <sbiscigl@amazon.com>
2019-12-12 10:44:39 -0500

    removing buggy cards

--- src/main/java/com/amazon/sbidoo/chance/ChanceCardBuilder.java
+++ src/main/java/com/amazon/sbidoo/chance/ChanceCardBuilder.java
@@ -19,30 +19,10 @@ final public class ChanceCardBuilder {
                         })
                         .build(),
                 Card.builder()
-                        .message("Go back three spaces")
-                        .cardApplier((player, banker) -> {
-                            int spacesBack  = player.getPositionFromStart() - 3;
-                            if (spacesBack >= 0) {
-                                player.setPositionFromStart(spacesBack);
-                            } else {
-                                player.setPositionFromStart(39 - Math.abs(spacesBack));
-                            }
-                            return "Go back three spaces. Current balance is " + player.getMoney() + " dollars";
-                        })
-                        .build(),
-                Card.builder()
                         .message("Speeding fine, pay 15 dollars")
                         .cardApplier((player, banker) -> {
                             player.setMoney(player.getMoney() - 15);
                             return "Speeding fine, pay 15 dollars. Current balance is " + player.getMoney() + " dollars";
-                        })
-                        .build(),
-                Card.builder()
-                        .message("Collect 200 dollars and for to start")
-                        .cardApplier((player, banker) -> {
-                            player.setPositionFromStart(0);
-                            player.setMoney(player.getMoney() + 200);
-                            return "Collect 200 dollars and for to start. Current balance is " + player.getMoney() + " dollars";
                         })
                         .build()
         );
